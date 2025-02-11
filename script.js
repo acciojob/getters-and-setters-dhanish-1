cd9e88e8b93662b6f71ca60d94b787caabf4a91d
Dhanish kumar <dhanishmca1@gmail.com>
2025-02-11 12:16:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
+}
+
+// Student class extending Person
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Teacher class extending Person
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
 
 // Do not change the code below this line
 window.Person = Person;
